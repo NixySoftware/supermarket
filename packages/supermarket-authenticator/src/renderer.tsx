0a72ecf25f63b9c1fa744b9cf02988145a57f4f7
Daniëlle Huisman <danielle@nixy.software>
2025-02-03 20:52:25 +0100

    feat(authenticator): add shadcn/ui

--- packages/supermarket-authenticator/src/renderer.tsx
+++ packages/supermarket-authenticator/src/renderer.tsx
@@ -1,4 +1,7 @@
 import { createRoot } from 'react-dom/client';
 
+import { App } from '~/app';
+import '~assets/css/index.css';
+
 const root = createRoot(document.body);
-root.render(<h2>Hello from React!</h2>);
+root.render(<App />);
