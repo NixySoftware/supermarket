ef03c26466d227da1b5fb52e76bfb680b2a5606a
Daniëlle Huisman <danielle@nixy.software>
2025-02-04 18:33:44 +0100

    feat(authenticator): add proof of concept (#42)

--- packages/supermarket-authenticator/src/main.ts
+++ packages/supermarket-authenticator/src/main.ts
@@ -2,6 +2,8 @@ import { BrowserWindow, app } from 'electron';
 import started from 'electron-squirrel-startup';
 import path from 'node:path';
 
+import { getCodeFromRedirectUrl } from './constants/supermarkets';
+
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (started) {
     app.quit();
@@ -9,17 +11,56 @@ if (started) {
 
 const createWindow = async () => {
     const mainWindow = new BrowserWindow({
-        width: 600,
+        width: 800,
         height: 600,
         webPreferences: {
             preload: path.join(__dirname, 'preload.js'),
         },
     });
 
+    mainWindow.webContents.addListener('will-navigate', (event) => {
+        intercept(mainWindow, event);
+    });
+
+    mainWindow.webContents.addListener('will-redirect', (event) => {
+        intercept(mainWindow, event);
+    });
+
+    await loadRenderer(mainWindow);
+};
+
+const loadRenderer = async (window: BrowserWindow, query: Record<string, string> = {}) => {
     if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
-        await mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
+        const url = new URL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
+        for (const [key, value] of Object.entries(query)) {
+            url.searchParams.set(key, value);
+        }
+        await window.loadURL(url.toString());
     } else {
-        await mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`));
+        await window.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`), {
+            query,
+        });
+    }
+};
+
+const intercept = (
+    window: BrowserWindow,
+    event:
+        | Electron.Event<Electron.WebContentsWillNavigateEventParams>
+        | Electron.Event<Electron.WebContentsWillRedirectEventParams>,
+) => {
+    console.log(event);
+
+    const result = getCodeFromRedirectUrl(event.url);
+    if (result) {
+        const [supermarket, code] = result;
+        console.log(supermarket, code);
+
+        event.preventDefault();
+        void loadRenderer(window, {
+            'supermarket-id': supermarket.id,
+            'supermarket-code': code,
+        });
     }
 };
 
